4685e277f356f0281cbee8e5c3959b6c17d78d85
kiesel <david.kesselheim@gmail.com>
2020-03-11 09:23:29 +0100

    fixed tag

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker build --build-arg POSTGIS_VERSION=3.0.0 \
              --build-arg TIMESCALEDB_VERSION=1.6.0 \
 	     --build-arg PG_VERSION=11 \
 	     --build-arg PREV_TS_VERSION=1.6.0 \
-	     -t timescaledb_postgis:1.0 .
+	     -t timescaledb-postgis:1.6.0-pg11 .
