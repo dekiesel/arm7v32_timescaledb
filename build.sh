5f622da3ad9ef6793d1e2fc5e12f8ebffb951013
kiesel <david.kesselheim@gmail.com>
2020-11-13 06:41:34 +0100

    bumped version numbers

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-docker build --build-arg POSTGIS_VERSION=3.0.0 \
-             --build-arg GDAL_VERSION=3.0.4 \
-             --build-arg PROJ_VERSION=7.0.0 \
-             --build-arg TIMESCALEDB_VERSION=1.6.0 \
-	     --build-arg PG_VERSION=11 \
-	     --build-arg PREV_TS_VERSION=1.6.0 \
-	     -t timescaledb-postgis:1.6.0-pg11 .
+docker build --build-arg POSTGIS_VERSION=3.0.2 \
+             --build-arg GDAL_VERSION=3.2.0 \
+             --build-arg PROJ_VERSION=7.0.1 \
+             --build-arg TIMESCALEDB_VERSION=1.7.4 \
+	     --build-arg PG_VERSION=12 \
+	     -t timescaledb-postgis:1.7.4-pg12 .
